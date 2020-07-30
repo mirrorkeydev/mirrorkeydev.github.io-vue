7854ea4c9bae7c4147ff0cae6411cb72b0f352b9
mirrorkeydev <mirrorkeydev@gmail.com>
2020-07-29 20:21:14 -0700

    Initial working commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,49 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <video autoplay loop muted>
+      <source src="../assets/Done-2.mp4" type="video/mp4">
+    </video>
+    <h1 id="title">melanie</h1>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name: 'Home'
 }
 </script>
+
+<style lang="scss">
+
+#title {
+  color: white;
+  font-size: 15vw;
+  position: relative;
+  height: 100vh;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0;
+  padding: 0;
+  user-select: none;
+  -moz-user-select: none;
+  -khtml-user-select: none;
+  -webkit-user-select: none;
+  -o-user-select: none;
+}
+
+video {
+  object-fit: cover;
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+  box-sizing: border-box;
+  border: white 5vw solid;
+  padding: 0;
+  margin: 0;
+}
+
+</style>
